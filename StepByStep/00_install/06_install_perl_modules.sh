998283988fc1a9766157e7b90792cb4789cdf8a2
Troy Will <troydwill@gmail.com>
2012-01-03 08:20:39 -0800

    modified:   06_install_perl_modules.sh

--- StepByStep/00_install/06_install_perl_modules.sh
+++ StepByStep/00_install/06_install_perl_modules.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -o verbose
 LOG="log.bundle"
-date >> ${LOG}
 # perl -MCPAN -e 'install Bundle::Interchange'
-cpanp i Bundle::Interchange > ${LOG} 2>&1
-date >> ${LOG}
+cpanp i Bundle::Interchange ${LOG} 2>&1 | tee ${LOG}
+
