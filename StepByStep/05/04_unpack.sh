f056c10592b82a1dbcd618f3da3c1bc0a65e1a94
Troy Will <troydwill@gmail.com>
2012-01-03 07:45:05 -0800

    modified:   StepByStep/05/06_install_perl_modules.sh

---
+++ StepByStep/05/04_unpack.sh
@@ -0,0 +1,2 @@
+tar -xf interchange-5.6.3.tar.bz2
+
--- StepByStep/05/06_install_perl_modules.sh
+++ StepByStep/05/06_install_perl_modules.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 set -o verbose
-
+LOG="log.bundle"
+date >> ${LOG}
 # perl -MCPAN -e 'install Bundle::Interchange'
-cpanp i Bundle::Interchange
+cpanp i Bundle::Interchange > ${LOG} 2>&1
+date >> ${LOG}
