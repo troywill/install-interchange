da289c9dd4a045bd326068c034a5415c150df039
Troy Will <troydwill@gmail.com>
2012-01-05 09:44:51 -0800

    renamed:    05_install_perl_modules.sh -> 05_install_bundle_interchange.sh

--- StepByStep/00_install/05_install_bundle_interchange.sh
+++ StepByStep/00_install/05_install_bundle_interchange.sh
@@ -3,7 +3,5 @@ set -o verbose
 LOG="log.bundle"
 # perl -MCPAN -e 'install Bundle::Interchange'
 # cpanp i Bundle::Interchange 2>&1 | tee ${LOG}
+pacman --sync perl-libwww
 cpanp i Bundle::Interchange
-
-
-
