a40ba6455a4048ab65958f8ff2867e491f0aa79c
Troy Will <troydwill@gmail.com>
2012-01-09 07:53:41 -0800

    modified:   00_install_perl_modules.pl

--- StepByStep/00_install/00_install_perl_modules.pl
+++ StepByStep/00_install/00_install_perl_modules.pl
@@ -2,41 +2,33 @@
 use warnings;
 use strict;
 
+my @modules = qw (
+		   perl-digest-sha1
+		   perl-mime-lite pacman
+		   perl-html-tagset
+		   perl-html-entities
+		   perl-bundle-lwp
+		   perl-libwww
+		   perl-parse-recdescent
+		   perl-ole-storage_lite
+		   perl-bundle-lwp
+		   perl-term-readkey
+		   perl-term-readline-perl
+		   perl-text-query
+		   perl-image-size
+		   perl-dbi
+		   perl-safe-hole
+		   perl-tie-shadowhash
+		   perl-set-crontab
+		   perl-io-scalar
+		   perl-storable
+		   perl-spreadsheet-parseexcel
+		   perl-spreadsheet-writeexcel
+		   perl-net-ip-match-regexp
+);
 
-#!/bin/sh
-
-
-# pacman --sync perl-digest-md5
-pacman --sync perl-digest-sha1
-# pacman --sync perl-mime-base64
-pacman --sync perl-mime-lite
-# pacman --sync perl-uri-url ( not found )
-pacman --sync perl-html-tagset
-pacman --sync perl-html-entities
-pacman --sync perl-bundle-lwp
-
-pacman --sync perl-libwww
-
-pacman --sync perl-parse-recdescent
-pacman --sync perl-ole-storage_lite
-pacman --sync perl-bundle-lwp
-pacman --sync perl-term-readkey
-pacman --sync perl-term-readline-perl
-pacman --sync perl-text-query
-pacman --sync perl-image-size
-pacman --sync perl-dbi
-pacman --sync perl-safe-hole
-pacman --sync perl-tie-shadowhash
-pacman --sync perl-set-crontab
-pacman --sync perl-io-scalar
-pacman --sync perl-storable
-pacman --sync perl-spreadsheet-parseexcel
-pacman --sync perl-spreadsheet-writeexcel
-pacman --sync perl-net-ip-match-regexp
-
-
-    perl-libwww: git svn
-    perl-term-readkey: git svn
-    perl-mime-tools: git send-email
-    perl-net-smtp-ssl: git send-email TLS support
-    perl-authen-sasl: git send-email TLS support
+foreach my $module ( @modules ) {
+  my $command = "pacman --sync $module";
+  print "==> $command\n";
+  system $command;
+}
