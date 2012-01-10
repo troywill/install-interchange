a0e7c7a18645c5bd622513cad2542f40611a84fb
Troy Will <troydwill@gmail.com>
2012-01-10 15:48:18 -0800

    new file:   00_email_01

---
+++ StepByStep/02_practical_configuration/04_delete_excess_tree.pl
@@ -0,0 +1,17 @@
+#!/usr/bin/env perl
+use warnings;
+use strict;
+use DBI;
+
+my $dbh = DBI->connect('DBI:mysql:interchange', 'troy', 'pass')
+    or die "Couldn't connect to database: " . DBI->errstr;
+
+&delete_excess_tree();
+
+sub delete_excess_tree {
+    my $sth_delete = $dbh->prepare('DELETE FROM tree WHERE code >= 35')
+	or die "Couldn't prepare statement: " . $dbh->errstr;
+
+    $sth_delete->execute();
+
+}
