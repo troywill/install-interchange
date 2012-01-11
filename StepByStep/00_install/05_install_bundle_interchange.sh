e6943bc5fa9ba188a84f808505717341bc756399
Troy Will <troydwill@gmail.com>
2012-01-11 07:31:14 -0800

    modified:   04_install_perl-text-query.sh

--- StepByStep/00_install/05_install_bundle_interchange.sh
+++ StepByStep/00_install/05_install_bundle_interchange.sh
@@ -3,5 +3,4 @@ set -o verbose
 LOG="log.bundle"
 # perl -MCPAN -e 'install Bundle::Interchange'
 # cpanp i Bundle::Interchange 2>&1 | tee ${LOG}
-pacman --sync perl-libwww
 cpanp i Bundle::Interchange
