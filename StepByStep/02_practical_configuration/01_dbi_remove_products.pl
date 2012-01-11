42fbd2a549e3b2080fe3d67832fea54392c6eaf3
Troy Will <troydwill@gmail.com>
2012-01-11 12:31:14 -0800

    modified:   02_tree.pl

--- StepByStep/02_practical_configuration/01_dbi_remove_products.pl
+++ StepByStep/02_practical_configuration/01_dbi_remove_products.pl
@@ -3,13 +3,13 @@ use warnings;
 use strict;
 use DBI;
 
-my $dbh = DBI->connect('DBI:mysql:interchange', 'troy', 'pass')
+my $dbh = DBI->connect('DBI:Pg:dbname=troy', 'troy')
     or die "Couldn't connect to database: " . DBI->errstr;
 
 &delete_products();
 
 sub delete_products {
-    my $sth_delete = $dbh->prepare('DELETE FROM products WHERE sku > "os28007"')
+    my $sth_delete = $dbh->prepare("DELETE FROM products WHERE sku > 'os28007'")
 	or die "Couldn't prepare statement: " . $dbh->errstr;
 
     $sth_delete->execute();
