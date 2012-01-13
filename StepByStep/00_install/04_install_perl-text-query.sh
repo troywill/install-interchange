c33684a8741912d3aeb44c745b37c85b579abe33
Troy Will <troydwill@gmail.com>
2012-01-13 07:21:11 -0800

    modified:   StepByStep/00_install/04_install_perl-text-query.sh

--- StepByStep/00_install/04_install_perl-text-query.sh
+++ StepByStep/00_install/04_install_perl-text-query.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 # https://rt.cpan.org/Public/Bug/Display.html?id=47252
 # https://rt.cpan.org/Ticket/Attachment/893884/463419/textquery.diff
-cpanp -i --skiptest Text::Query
-cpanp t Text::Query
-
-
+cpanp i --skiptest Text::Query
+# cpanp t Text::Query
 
