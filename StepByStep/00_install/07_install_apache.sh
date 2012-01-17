1e79e81439d5fd6db0a8383e903478993a3617fe
Troy Will <troydwill@gmail.com>
2012-01-17 07:32:42 -0800

    modified:   StepByStep/00_install/07_install_apache.sh

--- StepByStep/00_install/07_install_apache.sh
+++ StepByStep/00_install/07_install_apache.sh
@@ -3,4 +3,7 @@ pacman --sync apache
 emacs /etc/rc.conf
 chgrp troy /srv/http
 chmod g+w /srv/http
-mkdir /srv/http/cgi-bin && chgrp troy /srv/http/cgi-bin && chmod g+w /srv/http/cgi-bin
+mkdir --verbose /srv/http/cgi-bin
+chgrp --verbose troy /srv/http/cgi-bin && chmod --verbose g+w /srv/http/cgi-bin
+
+
